9ca3cfc1a900346afb62185a93868f5c0033f9cb
Wil Birkmaier <wil@birkmaier.org>
2014-10-27 22:35:54 -0400

    Changed to recognize local vs production server code

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,8 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('*'),
+	'local' => array('blackmirror'),
+	'production' => array('wilbirkmaier'),
 
 ));
 
