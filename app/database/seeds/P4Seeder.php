fd0b65da4e6fd23cf781cf52a6e71333f4a129ca
Wil Birkmaier <wil@birkmaier.org>
2014-12-06 16:11:18 -0500

    Modifed seeder to associate feeds with a master account

--- app/database/seeds/P4Seeder.php
+++ app/database/seeds/P4Seeder.php
@@ -14,11 +14,11 @@ class P4Seeder extends Seeder
         
             /*Add a test user*/
             $user = new User;
-            $user->username = 'tgmail';
-            $user->given = 'Test';
-            $user->sur = 'Gmail';
+            $user->username = 'opresent';
+            $user->given = 'Omni';
+            $user->sur = 'Present';
             $user->password = Hash::make('123');
-            $user->email = 'test@gmail.com';
+            $user->email = 'omni@present.com';
             $user->save();
         
             /*Add URLs for RSS Feeds to be displayed on default home page*/
@@ -27,102 +27,119 @@ class P4Seeder extends Seeder
             $feed->url = 'http://rss.slashdot.org/Slashdot/slashdot';
             $feed->maxresults = '10';
             $feed->save();
+            $feed->users()->attach($user);
         
             $feed = new Feed;
             $feed->name = 'slashDotLinux';
             $feed->url = 'http://rss.slashdot.org/Slashdot/slashdotlinux';
             $feed->maxresults = '10';
             $feed->save();
+            $feed->users()->attach($user);
         
             $feed = new Feed;
             $feed->name = 'bbcFeed';
             $feed->url = 'http://feeds.bbci.co.uk/news/rss.xml';
             $feed->maxresults = '10';
             $feed->save();
+            $feed->users()->attach($user);
         
             $feed = new Feed;
             $feed->name = 'slashDotHardware';
             $feed->url = 'http://rss.slashdot.org/Slashdot/slashdothardware';
             $feed->maxresults = '10';
             $feed->save();
+            $feed->users()->attach($user);
         
             $feed = new Feed;
             $feed->name = 'slashDotIt';
             $feed->url = 'http://rss.slashdot.org/Slashdot/slashdotit';
             $feed->maxresults = '10';
             $feed->save();
+            $feed->users()->attach($user);
         
             $feed = new Feed;
             $feed->name = 'cnnTopNews';
             $feed->url = 'http://rss.cnn.com/rss/cnn_topstories.rss';
             $feed->maxresults = '10';
             $feed->save();
+            $feed->users()->attach($user);
         
             $feed = new Feed;
             $feed->name = 'itWorldFeed';
             $feed->url = 'http://www.itworld.com/taxonomy/term/16/all/feed';
             $feed->maxresults = '10';
             $feed->save();
+            $feed->users()->attach($user);
         
             $feed = new Feed;
             $feed->name = 'nextWebFeed';
             $feed->url = 'http://thenextweb.com/feed/rss';
             $feed->maxresults = '10';
             $feed->save();
+            $feed->users()->attach($user);
         
             $feed = new Feed;
             $feed->name = 'xconomyItFeed';
             $feed->url = 'http://feeds.feedburner.com/Xconomy_IT';
             $feed->maxresults = '10';
             $feed->save();
+            $feed->users()->attach($user);
         
             $feed = new Feed;
             $feed->name = 'xconomyVcFeed';
             $feed->url = 'http://feeds.feedburner.com/Xconomy_VC';
             $feed->maxresults = '10';
             $feed->save();
+            $feed->users()->attach($user);
         
             $feed = new Feed;
             $feed->name = 'linuxJournalFeed';
             $feed->url = 'http://feeds.feedburner.com/linuxjournalcom';
             $feed->maxresults = '10';
             $feed->save();
+            $feed->users()->attach($user);
         
             $feed = new Feed;
             $feed->name = 'techRepublicFeed';
             $feed->url = 'http://www.techrepublic.com/rssfeeds/articles/latest/';
             $feed->maxresults = '10';
             $feed->save();
+            $feed->users()->attach($user);
         
             $feed = new Feed;
             $feed->name = 'cioFeed';
             $feed->url = 'http://feeds.cio.com/cio/feed/articles';
             $feed->maxresults = '10';
             $feed->save();
+            $feed->users()->attach($user);
         
             $feed = new Feed;
             $feed->name = 'networkWorldFeed';
             $feed->url = 'http://www.networkworld.com/rss';
             $feed->maxresults = '10';
             $feed->save();
+            $feed->users()->attach($user);
         
             $feed = new Feed;
             $feed->name = 'bizJournalFeed';
             $feed->url = 'http://feeds.bizjournals.com/bizj_boston';
             $feed->maxresults = '10';
             $feed->save();
+            $feed->users()->attach($user);
         
             $feed = new Feed;
             $feed->name = 'ycFeed';
             $feed->url = 'https://news.ycombinator.com/rss';
             $feed->maxresults = '10';
             $feed->save();
+            $feed->users()->attach($user);
             
             $feed = new Feed;
             $feed->name = 'PBSFeed';
             $feed->url = 'http://feeds.feedburner.com/pbs/wgbh/frontline/programs-feed';
             $feed->maxresults = '10';
             $feed->save();
+            $feed->users()->attach($user);
             
 
         }   
