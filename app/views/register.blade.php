59556b843fdc0676a2183b636e8be374020adacd
Wil Birkmaier <wil@birkmaier.org>
2014-12-06 17:44:32 -0500

    Changed placeholders

--- app/views/register.blade.php
+++ app/views/register.blade.php
@@ -23,16 +23,16 @@
 
     <form action="{{ url('register') }}" method="post">
         <p><label for="username">Username:</label></p>
-        <p><input type="text" name="username" placeholder="rdoe"/></p>
+        <p><input type="text" name="username" placeholder="ddoe"/></p>
         
         <p><label for="given">Name:</label></p>
-        <p><input type="text" name="given" placeholder="Robin"/></p>
+        <p><input type="text" name="given" placeholder="Deer"/></p>
         
         <p><label for="sur">Last Name:</label></p>
         <p><input type="text" name="sur" placeholder="Doe"/></p>
         
         <p><label for="email">Email:</label></p>
-        <p><input type="text" name="email" placeholder="robin@doe.com"/></p>
+        <p><input type="text" name="email" placeholder="deer@doe.com"/></p>
         
         <p><label for="password">Password:</label></p>
         <p><input type="password" name="password" placeholder="Secret Password"/></p>
