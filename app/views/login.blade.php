6722d4da7282d2a268c558af4e19e33f174be468
Wil Birkmaier <wil@birkmaier.org>
2014-12-06 21:58:50 -0500

    Changed placeholder

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -12,7 +12,7 @@
 
     <form method="post" action="{{ url('login') }}" >
         <p><label for="email">Email:</label></p>
-        <p><input class="form-control" type="text" name="email" placeholder="robin@doe.com"/></p>
+        <p><input class="form-control" type="text" name="email" placeholder="deer@doe.com"/></p>
         <p><label for="password">Password:</label></p>
         <p><input class="form-control" type="password" name="password" placeholder="Secret Password" /></p>
         <p><input type="submit" value="Login" class="btn btn-primary"/></p>
