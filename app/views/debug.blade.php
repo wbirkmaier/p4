750a2514b44a068e6d743c92601c098d062ab216
Wil Birkmaier <wil@birkmaier.org>
2014-12-06 23:53:34 -0500

    clean up debug page to nothing

--- app/views/debug.blade.php
+++ app/views/debug.blade.php
@@ -16,44 +16,5 @@
 @section('content')
 
 <h1>debug!</h1>
-
-//Test Code//<br>
-
-<a href="{{ action('IndexController@createFeed') }}" class="btn btn-primary">Create Feed</a>
-
-@if ($feeds->isEmpty())
-    <p>No Feed Records Found</p>
-@else
-    <table class="table table-striped">
-        <thead>
-            <tr>
-                <th>Name</th>
-                <th>URL</th>
-                <th>Max Results</th>
-                <th>Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-      
-@foreach($feeds as $feed)
-        <tr>
-            <td>{{ $feed->name }}</td>
-            <td>{{ $feed->url }}</td>
-            <td>{{ $feed->maxresults }}</td>
-            <td>
-                <a href="{{ action('IndexController@changeFeed', $feed->id) }}"class="btn btn-primary">Modify</a>
-                <a href="{{ action('IndexController@deleteFeed', $feed->id) }}" class="btn btn-danger">Delete</a>
-            </td>
-        </tr>
-@endforeach
-
-        </tbody>
-    </table>
-
-@endif
-
-<a href="{{ action('IndexController@getLogout') }}" class="btn btn-primary">Logout</a>
-
-//End Test Code//
-
+ <!-- place debug code here -->
 @stop
