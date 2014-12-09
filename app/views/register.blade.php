0ab873705e99abca40fe643e8dea12f2da9167c3
Wil Birkmaier <wil@birkmaier.org>
2014-12-09 14:28:01 -0500

    Fixed form fill on error

--- app/views/register.blade.php
+++ app/views/register.blade.php
@@ -16,10 +16,12 @@
 
 @section('content')
 
+<!-- Error message when id or email is taken-->
 @foreach ($errors->all() as $message)
-    <div class='error'>{{ $message }}</div>
+    <div class='error col-md-3 col-md-offset-4'>{{ $message }}</div>
     <br>
 @endforeach
+
  <!-- Center form with bootstrap doing (12 - 3) / 2 = ~4 -->
  <div class="col-md-3 col-md-offset-4">
 
@@ -30,22 +32,30 @@
         <!-- Allow for fancy font awesome bootstrap icons for all fields via classes-->
         <div class="input-group margin-bottom-sm">
             <span class="input-group-addon"><i class="fa fa-keyboard-o fa-fw"></i></span>
-            <input class="form-control" type="text" name="username" placeholder="your username"/>
+            
+            <!--<input placeholder="your username" class="form-control" name="username" type="text" value=""/>-->
+            {{ Form::text('username', '', array('placeholder'=>'your username', 'class'=>'form-control')) }}
         </div>
         <br>
         <div class="input-group">
             <span class="input-group-addon"><i class="fa fa-user fa-fw"></i></span>
-            <input class="form-control" type="text" name="given" placeholder="your first name"/>
+            
+            <!--<input placeholder="your firstname" class="form-control" name="given" type="text" value=""/>-->
+            {{ Form::text('given', '', array('placeholder'=>'your first name', 'class'=>'form-control')) }}
         </div>
         <br>
         <div class="input-group">
             <span class="input-group-addon"><i class="fa fa fa-users fa-fw"></i></span>
-            <input class="form-control" type="text" name="sur" placeholder="your surname"/>
+            
+            <!--<input placeholder="your surname" class="form-control" name="sur" type="text" value=""/>-->
+            {{ Form::text('sur', '', array('placeholder'=>'your surname', 'class'=>'form-control')) }}
         </div>
         <br>
         <div class="input-group">
             <span class="input-group-addon"><i class="fa fa-envelope-o fa-fw"></i></span>
-            <input class="form-control" type="text" name="email" placeholder="your email"/>
+            
+            <!--<input placeholder="your email" class="form-control" name="email" type="text" value=""/>-->
+            {{ Form::text('email', '', array('placeholder'=>'your email', 'class'=>'form-control')) }}
         </div>
         <br>
         <div class="input-group">
