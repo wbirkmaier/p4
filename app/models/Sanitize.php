ef583e11d5deb0f2dc1d9777edb718a216248bad
Wil Birkmaier <wil@birkmaier.org>
2014-12-09 15:45:59 -0500

    Added class to sanitize strings from users

---
+++ app/models/Sanitize.php
@@ -0,0 +1,35 @@
+<?php
+
+class Sanitize extends Eloquent
+    {   
+        /*Set Global Properties (variable) for all Methods (Functions) */
+        public $userInput;
+        
+        /*Method (function) to get the variable into the object*/
+        public function setSanitize($userInput)
+            {
+                $this->userInput = $userInput;
+            }
+    
+        /*Method to return the sanitized string*/
+        public function getSanitize()
+            {
+                /*Code to strip any strange characters from user input*/
+                $sanitize = $this->userInput;
+                
+                /*Set to US character set*/
+                setlocale(LC_ALL, 'en_US.UTF8');
+
+                /*Delimeter to replace spaces and other non normal characters*/
+                $delimiter = '-';
+                        
+                /*Regex to remove special characters, etc*/
+                $sanitize = iconv('UTF-8', 'ASCII//TRANSLIT', $sanitize);
+                $sanitize = preg_replace("/[^a-zA-Z0-9\/_|+ -]/", '', $sanitize);
+                $sanitize = preg_replace("/[\/_|+ -]+/", $delimiter, $sanitize);
+                $sanitize = strtolower(trim($sanitize, $delimiter));
+            
+		        return $sanitize;
+	         }
+    
+    }
